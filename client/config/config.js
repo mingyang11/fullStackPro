547e316a4a9463107e5a49421da8f85dd4969a66
mingyang11 <2578349261@qq.com>
2019-06-18 00:40:01 +0800

    再次提交

--- client/config/config.js
+++ client/config/config.js
@@ -2,7 +2,7 @@ export default {
     singular: true,
 
     plugins: [
-        // 这里配置的作用是使用antd的样式的按需加载功能
+        // 这里配置的作用是使用antd的样式的按需加载功能xe
         ['umi-plugin-react', {
             antd: true,
             dva: true,
