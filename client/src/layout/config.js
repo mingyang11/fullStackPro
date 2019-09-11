30b37cbfe22f023fdcd6f50c373433d2561c1488
mingyang11 <2578349261@qq.com>
2019-09-12 00:20:02 +0800

    删除文件

--- client/src/layout/config.js
+++ client/src/layout/config.js
@@ -1,35 +1,40 @@
 const routeConfig = [
-    {
-        title: '首页',
-        path: '/helloWorld',
-        key: 'helloWorld',
-    },
-    {
-        title: '测试主页',
-        key: 'dashboard',
-        children: [
-            {
-                title: '分析页',
-                path: '/dashboard/analysis',
-                key: 'analysis',
-            },
-            {
-                title: '监控',
-                path: '/dashboard/monitor',
-                key: 'monitor',
-            },
-            {
-                title: '工作区',
-                path: '/dashboard/workplace',
-                key: 'workplace',
-            },
-        ]
-    },
-    {
-        title: '卡片页',
-        path: '/cardPage',
-        key: 'cardPage',
-    }
+  {
+    title: '首页',
+    path: '/helloWorld',
+    key: 'helloWorld',
+    icon: 'user'
+  },
+  {
+    title: '测试主页',
+    icon: 'user',
+    children: [
+      {
+        title: '分析页',
+        path: '/dashboard/analysis',
+        key: 'analysis',
+        icon: 'user'
+      },
+      {
+        title: '监控',
+        path: '/dashboard/monitor',
+        key: 'monitor',
+        icon: 'user'
+      },
+      {
+        title: '工作区',
+        path: '/dashboard/workplace',
+        key: 'workplace',
+        icon: 'user'
+      }
+    ]
+  },
+  {
+    title: '卡片页',
+    path: '/cardPage',
+    key: 'cardPage',
+    icon: 'user'
+  }
 ]
 
-export default routeConfig;
+export default routeConfig
--- client/src/layout/index.js
+++ client/src/layout/index.js
@@ -1,83 +1,92 @@
-import React, {Component} from 'react';
-import { Layout, Menu, Icon } from 'antd';
-import Link from 'umi/link';
-import routeConfig from './config';
+import React, { Component } from 'react'
+import { Layout, Menu, Icon } from 'antd'
+import Link from 'umi/link'
+import routeConfig from './config'
 
-const { Header, Sider, Content } = Layout;
-const SubMenu = Menu.SubMenu; 
+const { Header, Sider, Content } = Layout
+const SubMenu = Menu.SubMenu
 
 class BasicLayout extends Component {
-    constructor(props) {
-        super(props);
-        this.state={
-            current: ['helloWorld'],
-            collapsed: false,
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      current: ['helloWorld'],
+      collapsed: false
     }
-    componentDidMount() {
-        
-    }
-    renderMenu = (data) => {
-        return data.map((element) => {
-            if(element.children) {
-                return (
-                    <SubMenu title={<span><Icon type="user" />{element.title}</span>} key={element.key}>
-                        {this.renderMenu(element.children)}
-                    </SubMenu>
-                )
-            }
-            return <Menu.Item key={element.key} title={element.title}>
-                <Link to={element.path}><Icon type="user" />{element.title}</Link>
-            </Menu.Item>
-        })
-    }
-    handleClick = (e) => {
-        this.setState({
-            current: e.keyPath
-        })
-    }
-    toggle = () => {
-        this.setState({
-          collapsed: !this.state.collapsed,
-        });
-    }
-    render() {
-        const { children } = this.props;
-        const { current } = this.state;
+  }
+  componentDidMount() {}
+
+  renderMenu = data => {
+    return data.map(element => {
+      if (element.children) {
         return (
-            <Layout>
-                <Sider collapsible collapsed={this.state.collapsed}>
-                    <div style={{height: 100}} />
-                    <Menu
-                        onClick={this.handleClick}
-                        theme="dark"
-                        mode="inline"
-                        defaultSelectedKeys={current}
-                    >
-                        {
-                            this.renderMenu(routeConfig)
-                        }
-                    </Menu>
-                </Sider>
-                <Layout>
-                    <Header style={{ background: '#fff', padding: 0 }}>
-                        
-                    </Header>
-                    <Content
-                        style={{
-                            margin: '24px 16px',
-                            padding: 24,
-                            background: '#fff',
-                            minHeight: window.innerHeight,
-                        }}
-                    >
-                        <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>{children}</div>
-                    </Content>
-                </Layout>
-            </Layout>
-            
+          <SubMenu
+            title={
+              <span>
+                <Icon type={element.icon} />
+                {element.title}
+              </span>
+            }
+            key={element.key}
+          >
+            {this.renderMenu(element.children)}
+          </SubMenu>
         )
-    }
+      }
+      return (
+        <Menu.Item key={element.key} title={element.title}>
+          <Link to={element.path}>
+            <Icon type={element.icon} />
+            {element.title}
+          </Link>
+        </Menu.Item>
+      )
+    })
+  }
+  handleClick = e => {
+    this.setState({
+      current: e.keyPath
+    })
+  }
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    })
+  }
+  render() {
+    const { children } = this.props
+    const { current } = this.state
+    return (
+      <Layout>
+        <Sider>
+          <div style={{ height: 100 }} />
+          <Menu
+            onClick={this.handleClick}
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={current}
+          >
+            {this.renderMenu(routeConfig)}
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }} />
+          <Content
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              background: '#fff',
+              minHeight: window.innerHeight
+            }}
+          >
+            <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+              {children}
+            </div>
+          </Content>
+        </Layout>
+      </Layout>
+    )
+  }
 }
 
-export default BasicLayout;
+export default BasicLayout
