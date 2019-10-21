0400231f5640696e86ef3d148d3437fba65618b7
mingyang11 <2578349261@qq.com>
2019-10-21 23:09:04 +0800

    配置文件修改

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,21 +1,3 @@
-import { notification } from 'antd'
-
-const v2 = '/api/v2'
-const v3 = '/api/v3'
-const checkCode = response =>
-  response.then(data => {
-    const { Success } = data
-    switch (Success) {
-      case true:
-        return data
-      case false:
-        notification.error({
-          message: data.ErrorMessage
-        })
-        return data
-      default:
-        return data
-    }
-  })
-
-export { v2, v3, checkCode }
+export function isAntdPro() {
+  return window.location.hostname === 'preview.pro.ant.design'
+}
