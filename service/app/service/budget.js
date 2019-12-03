2f30aa2289d5eb3f00d50427bde5110094cd4ec3
mingyang11 <2578349261@qq.com>
2019-12-03 22:22:44 +0800

    我也不知道添加了啥

--- service/app/service/budget.js
+++ service/app/service/budget.js
@@ -5,7 +5,6 @@ const { Service } = require('egg');
 class UserService extends Service {
   async addBudget(params) {
     const budgetInfo = await this.ctx.model.Budget.create(params);
-    console.log(budgetInfo, 'budgetInfo');
     return budgetInfo;
   }
 }
