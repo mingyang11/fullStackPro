cc72a4f36d296269edeeabfc87ca23e8e5439b61
Mingyang11 <2578349261@qq.com>
2020-04-08 21:48:02 +0800

    又分手了，嘿嘿

--- client/src/pages/JestLearn/index.js
+++ client/src/pages/JestLearn/index.js
@@ -1,7 +1,7 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent } from "react";
 
 export default class JestLearn extends PureComponent {
   render() {
-    return <div className="div">hello</div>
+    return <div className="div">hello</div>;
   }
 }
